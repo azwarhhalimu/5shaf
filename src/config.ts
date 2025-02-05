bdb1f0edf0a3436e1918feb1275fd297785c9d85
AZWAR HALIMU <azwarhalimu@gmail.com>
2025-02-05 13:18:55 +0800

    akhir

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
-//let host: string = "http://localhost:2001";
-let host: string = "https://api.azwarhalimu.biz.id";
+let host: string = "http://localhost:2001";
+// let host: string = "https://api.azwarhalimu.biz.id";
 const baseUrl = (url: string): string => {
   let result: string = host + "/" + url;
   return result;
